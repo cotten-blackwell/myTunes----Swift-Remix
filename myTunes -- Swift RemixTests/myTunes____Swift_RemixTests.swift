ed87c16931f3aba4148c3a4dbb221cfd9724bdd8
cotten-blackwell <cotten.blackwell@gmail.com>
2015-11-07 18:41:49 -0800

    Initial Commit

---
+++ myTunes -- Swift RemixTests/myTunes____Swift_RemixTests.swift
@@ -0,0 +1,36 @@
+//
+//  myTunes____Swift_RemixTests.swift
+//  myTunes -- Swift RemixTests
+//
+//  Created by Cotten Blackwell on 11/7/15.
+//  Copyright © 2015 Cotten Blackwell. All rights reserved.
+//
+
+import XCTest
+@testable import myTunes____Swift_Remix
+
+class myTunes____Swift_RemixTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
